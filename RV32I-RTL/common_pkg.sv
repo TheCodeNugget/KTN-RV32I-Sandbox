66f739e745ce1670b4d6533c06fb2bb08318d289
TheCodeNugget <95768094+TheCodeNugget@users.noreply.github.com>
2026-07-16 20:09:14 +0300

    ✨controller added

--- RV32I-RTL/common_pkg.sv
+++ RV32I-RTL/common_pkg.sv
@@ -1,5 +1,5 @@
 /// --------------------------------------------------------
-/// instr_decoder.sv
+/// common_pkg.sv
 /// Ken The Nugget
 /// 14/07/2026
 /// RV32I Common ENUMS, Structs, etc
@@ -7,6 +7,7 @@
 
 package common_pkg;
 
+    // Instruction Types
     typedef enum logic [6:0] {  
         R_TYPE      = 7'h33;
         I_TYPE_0    = 7'h03;
@@ -19,6 +20,7 @@ package common_pkg;
         J_TYPE      = 7'h6F;
     } riscv_op_t;
 
+    // ALU Operations
     typedef enum logic [3:0] {
         ALU_ADD,
         ALU_SUB,
@@ -32,11 +34,95 @@ package common_pkg;
         ALU_SLT
     } alu_op_t;
 
+    // Memory Access Types
     typedef enum logic [1:0] {
         BYTE,
         HALF,
         RSVD,
         WORD
     } mem_access_t;
+
+    // R-Type Instructions
+    // Format: {funct7[5], funct3}
+    typedef enum logic [3:0] {
+        ADD		= 4'b0000,
+        SUB		= 4'b1000,
+        SLL		= 4'b0001,
+        SLT		= 4'b0010,
+        SLTU 	= 4'b0011,
+        XOR		= 4'b0100,
+        SRL		= 4'b0101,
+        SRA		= 4'b1101,
+        OR		= 4'b0110,
+        AND		= 4'b0111
+    } r_inst_t;
+
+    // I-Type Instruction
+    // Format: {opcode[4], funct3}
+    typedef enum logic [3:0] {
+        LB		= 4'b0000,
+        LH		= 4'b0001,
+        LW		= 4'b0010,
+        LBU		= 4'b0100,
+        LHU		= 4'b0101,
+        ADDI	= 4'b1000,
+        SLTI	= 4'b1010,
+        SLTIU	= 4'b1011,
+        XORI	= 4'b1100,
+        ORI		= 4'b1110,
+        ANDI	= 4'b1111,
+        SLLI	= 4'b1001,
+        SRXI	= 4'b1101
+    } i_inst_t;
+  
+    // S-Type Instruction
+    typedef enum logic [2:0] {
+        SB		= 3'b000,
+        SH		= 3'b001,
+        SW		= 3'b010
+    } s_inst_t;
+  
+    // B-Type Instruction
+    typedef enum logic [2:0] {
+        BEQ		=	3'b000,
+        BNE		=	3'b001,
+        BLT		=	3'b100,
+        BGE		=	3'b101,
+        BLTU	=	3'b110,
+        BGEU	=	3'b111
+    } b_inst_t;
+  
+    // U-Type Instruction
+    typedef enum logic [6:0] {
+        AUIPC	= 7'b0010111,
+        LUI		= 7'b0110111
+    } u_inst_t;
+  
+    // j-Type Instruction
+    typedef enum logic [5:0] {
+        JAL 	= 6'h3
+    } j_inst_t;
+  
+    // Register File Write Source
+    typedef enum logic [1:0] {
+        ALU,
+        MEM,
+        IMM,
+        PC
+    } rf_wr_src_t;
+  
+    // Instruction Control Packet
+    typedef struct packed {
+        logic 		pc_sel;
+        logic		op1_sel;
+        logic		op2_sel;
+        logic		data_req;
+        logic		data_wr;
+        logic		zero_extnd;
+        logic		rf_wr_en;
+        logic [1:0]	data_byte;
+        logic [1:0]	rf_wr_src;
+        logic [3:0] alu_func;
+    } ctrl_packet_t;
 
 endpackage
---
+++ RV32I-RTL/controller.sv
@@ -0,0 +1,200 @@
+/// --------------------------------------------------------
+/// controller.sv
+/// Ken The Nugget
+/// 16/07/2026
+/// RV32I Control Unit
+/// --------------------------------------------------------
+
+module controller import common_pkg::*; (
+    // Instruction Type
+    input   logic   r_type_i,
+    input   logic   i_type_i,
+    input   logic   s_type_i,
+    input   logic   b_type_i,
+    input   logic   u_type_i,
+    input   logic   j_type_i,
+
+    // Instruction Fields
+    input   logic       funct7_bit5_i,
+    input   logic [2:0] funct3_i,
+    input   logic [6:0] opcode_i,
+
+    // Control Interface
+    output  logic       pc_sel_o,       // PC MUX Select ? JMP TGT : PC + 4
+    output  logic       op1_sel_o,      // ALU OP-A MUX Select ? PC : RS1
+    output  logic       op2_sel_o,      // ALU OP-B MUX Select ? IMM : RS2
+    output  logic       data_req_o,     // Data Memory Request Flag
+    output  logic       data_wr_o,      // Data Memory WR/RD Select
+    output  logic       zero_extnd_o,   // ZERO Extend Flag for Data Memory
+    output  logic       rf_wr_en,       // Register File WR/RD Select
+    output  logic [1:0] data_byte,      // Data RD/WR Length Select
+    output  logic [1:0] rf_wr_src,      // Register File Write Data Source Select
+    output  logic [3:0] alu_func        // ALU Function Select
+);
+
+
+    // --------------------------------------------------------
+    // Internal Signal List
+    // --------------------------------------------------------
+    logic [3:0] r_instr_code;
+    logic [3:0] i_instr_code;
+    ctrl_packet_t r_instr_ctrl;
+    ctrl_packet_t i_instr_ctrl;
+    ctrl_packet_t s_instr_ctrl;
+    ctrl_packet_t b_instr_ctrl;
+    ctrl_packet_t u_instr_ctrl;
+    ctrl_packet_t j_instr_ctrl;
+    ctrl_packet_t mux_instr_ctrl;
+
+    // --------------------------------------------------------
+    // R-Type Handling
+    // --------------------------------------------------------
+    assign r_instr_code = {funct7_bit5_i, funct3_i};
+    always_comb begin
+        r_instr_ctrl = '0;
+        r_instr_ctrl.rf_wr_en = 1'b1;
+        case (r_instr_code)
+            ADD:        r_instr_code.alu_func = ALU_ADD;
+            SUB:        r_instr_code.alu_func = ALU_SUB;
+            SLL:        r_instr_code.alu_func = ALU_SLL;
+            SLT:        r_instr_code.alu_func = ALU_SLT;
+            SLTU:       r_instr_code.alu_func = ALU_SLTU;
+            XOR:        r_instr_code.alu_func = ALU_XOR;
+            SRL:        r_instr_code.alu_func = ALU_SRL;
+            SRA:        r_instr_code.alu_func = ALU_SRA;
+            OR:         r_instr_code.alu_func = ALU_OR;
+            AND:        r_instr_code.alu_func = ALU_AND;
+            default:    r_instr_code.alu_func = ALU_ADD;
+        endcase
+    end
+
+    // --------------------------------------------------------
+    // I-Type Handling
+    // --------------------------------------------------------
+    assign i_instr_code = {opcode_i[4], funct3_i};
+    always_comb begin
+        i_instr_ctrl = '0;
+        i_instr_ctrl.rf_wr_en = 1'b1;
+        case (i_instr_code)
+            LB: begin
+                i_instr_ctrl.data_req   = 1'b1;
+                i_instr_ctrl.data_byte  = BYTE;
+                i_instr_ctrl.rf_wr_src  = MEM;
+            end
+            LH: begin
+                i_instr_ctrl.data_req   = 1'b1;
+                i_instr_ctrl.data_byte  = HALF;
+                i_instr_ctrl.rf_wr_src  = MEM;
+            end
+            LW: begin
+                i_instr_ctrl.data_req   = 1'b1;
+                i_instr_ctrl.data_byte  = WORD;
+                i_instr_ctrl.rf_wr_src  = MEM;
+            end
+            LBU: begin
+                i_instr_ctrl.data_req 	= 1'b1;
+                i_instr_ctrl.data_byte  = BYTE;
+                i_instr_ctrl.rf_wr_src  = MEM;
+                i_instr_ctrl.zero_extnd = 1'b1;
+            end
+            LHU: begin
+                i_instr_ctrl.data_req   = 1'b1;
+                i_instr_ctrl.data_byte  = HALF;
+                i_instr_ctrl.rf_wr_src  = MEM;
+                i_instr_ctrl.zero_extnd = 1'b1;
+            end
+            ADDI:		i_instr_ctrl.alu_func = ALU_ADD;
+            SLTI:		i_instr_ctrl.alu_func = ALU_SLT;
+            SLTIU:		i_instr_ctrl.alu_func = ALU_SLTU;
+            XORI:		i_instr_ctrl.alu_func = ALU_XOR;
+            ORI:		i_instr_ctrl.alu_func = ALU_OR;
+            ANDI:		i_instr_ctrl.alu_func = ALU_AND;
+            SLLI:		i_instr_ctrl.alu_func = ALU_SLL;
+            SRXI:	    i_instr_ctrl.alu_func = (instr_funct7_bit5_i) ? ALU_SRA : ALU_SRL;
+            default:    i_instr_ctrl = '0;
+        endcase
+
+        if ((instr_opcode_i == I_TYPE_2)) begin
+            i_instr_ctrl.rf_wr_src  = PC;
+            i_instr_ctrl.pc_sel     = 1'b1;
+            i_instr_ctrl.alu_func   = ALU_ADD;
+        end  
+    end
+
+    // --------------------------------------------------------
+    // S-Type Handling
+    // --------------------------------------------------------
+    always_comb begin
+        s_instr_ctrl            = '0;
+        s_instr_ctrl.data_req   = 1'b1;
+        s_instr_ctrl.data_wr	= 1'b1;
+        s_instr_ctrl.op2_sel	= 1'b1;
+        case (instr_funct3_i)
+            SB:	        s_instr_ctrl.data_byte = BYTE;
+            SH:	        s_instr_ctrl.data_byte = HALF;
+            SW:	        s_instr_ctrl.data_byte = WORD;
+            default:    s_instr_ctrl = '0;
+        endcase
+    end
+
+    // --------------------------------------------------------
+    // B-Type Handling
+    // --------------------------------------------------------
+    always_comb begin
+        b_instr_ctrl            = '0;
+        b_instr_ctrl.alu_func   = ALU_ADD;
+        b_instr_ctrl.op1_sel	= 1'b1;
+        b_instr_ctrl.op2_sel	= 1'b1;
+    end
+
+    // --------------------------------------------------------
+    // U-Type Handling
+    // --------------------------------------------------------
+    always_comb begin
+        u_instr_ctrl            = '0;
+        u_instr_ctrl.rf_wr_en   = 1'b1;
+        case (instr_opcode_i)
+            AUIPC:   	{u_instr_ctrl.op2_sel, u_instr_ctrl.op1_sel} = {1'b1, 1'b1};
+            LUI:   		u_instr_ctrl.rf_wr_src = IMM;
+            default:    u_instr_ctrl = '0;
+        endcase
+    end
+
+    // --------------------------------------------------------
+    // J-Type Handling
+    // --------------------------------------------------------
+    always_comb begin
+        j_instr_ctrl            = '0;
+        j_instr_ctrl.rf_wr_en   = 1'b1;
+        j_instr_ctrl.rf_wr_src  = PC;
+        j_instr_ctrl.op2_sel    = 1'b1;
+        j_instr_ctrl.op1_sel    = 1'b1;
+        j_instr_ctrl.pc_sel     = 1'b1;
+    end
+
+    // --------------------------------------------------------
+    // Instruction Control Multiplexing
+    // --------------------------------------------------------
+    assign mux_instr_ctrl = is_r_type_i ? r_instr_ctrl :
+                            is_i_type_i ? i_instr_ctrl :
+                            is_s_type_i ? s_instr_ctrl :
+                            is_b_type_i ? b_instr_ctrl :
+                            is_u_type_i ? u_instr_ctrl :
+                            is_j_type_i ? j_instr_ctrl :
+                                        '0;
+
+    // --------------------------------------------------------
+    // Output Assignments
+    // --------------------------------------------------------
+    assign pc_sel_o     = mux_instr_ctrl.pc_sel;
+    assign op1sel_o     = mux_instr_ctrl.op1_sel;
+    assign op2sel_o     = mux_instr_ctrl.op2_sel;
+    assign alu_func_o   = mux_instr_ctrl.alu_func;
+    assign rf_wr_en_o   = mux_instr_ctrl.rf_wr_en;
+    assign data_req_o   = mux_instr_ctrl.data_req;
+    assign data_byte_o  = mux_instr_ctrl.data_byte;
+    assign data_wr_o    = mux_instr_ctrl.data_wr;
+    assign zero_extnd_o = mux_instr_ctrl.zero_extnd;
+    assign rf_wr_data_o = mux_instr_ctrl.rf_wr_src;
+
+endmodule
